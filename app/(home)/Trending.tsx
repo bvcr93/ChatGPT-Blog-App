c41b0e0f04523494300535b9e7bf5f075c7d4e96
bvcr93 <darebavcar1@gmail.com>
2023-04-24 23:28:41 +0200

    trending section/trending card component

--- app/(home)/Trending.tsx
+++ app/(home)/Trending.tsx
@@ -1,7 +1,7 @@
+import Link from "next/link";
 import React from "react";
-type TrendingCardProps = {
-  className?: string;
-};
+import Image from "next/image";
+
 export default function Trending() {
   return (
     <section className="pt-3 pb-10 ">
@@ -17,17 +17,47 @@ export default function Trending() {
           sapiente.
         </p>
       </div>
-      <div className="flex justify-between gap-3 my-3 ">
-        <div className="basis-1/2 bg-wh-50 h-96 border"></div>
+      {/* <div className="flex justify-between gap-3 my-3 ">
+        <div className="basis-1/2 bg-wh-500 h-96 border"></div>
         <div className="basis-1/2 gap-3 bg-wh-50 h-96 border flex flex-col">
           <div className="basis-1/2 bg-wh-500"></div>
           <div className="basis-1/2 flex gap-3 ">
             <div className="basis-1/2 bg-wh-500"></div>
             <div className="basis-1/2 bg-wh-500"></div>
           </div>
-          <div></div>
         </div>
+      </div> */}
+      <div className="sm:grid grid-cols-4 gap-5 grid-rows-2 sm:h-[600px] my-3">
+        <TrendingCard className=" col-span-2 row-span-2  bg-wh-10"></TrendingCard>
+        <TrendingCard className=" col-span-2 row-span-1  bg-wh-10"></TrendingCard>
+        <TrendingCard className=" col-span-1 row-span-1  bg-wh-10"></TrendingCard>
+        <TrendingCard className=" col-span-1 row-span-1  bg-wh-10"></TrendingCard>
       </div>
     </section>
   );
 }
+
+// TRENDING CARD COMPONENT
+
+type TrendingCardProps = {
+  className?: string;
+};
+
+const TrendingCard = ({ className }: TrendingCardProps) => {
+  return (
+    <Link
+      href="/"
+      className={`${className} sm:mt-0 sm:h-auto relative mt-7 block w-full h-96 hover:opacity-70`}
+      //   href={`${process.env.NEXT_PUBLIC_URL}/post/${post?.id}`}
+    >
+      <div className="z-0 relative w-full h-full">image</div>
+      <div className="absolute z-1 top-0 left-0 w-full h-full bg-gradient-gradual" />
+      <div className="absolute z-2 bottom-0 left-0 p-3">
+        <h4 className="inline-block px-5 py-1 font-semibold bg-accent-orange text-wh-900">
+          dfg
+        </h4>
+        <div className="text-wh-100 mt-2">dfg</div>
+      </div>
+    </Link>
+  );
+};
