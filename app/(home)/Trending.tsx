dc83224563e5224f464f5d5a0ead2bd6542cd974
bvcr93 <darebavcar1@gmail.com>
2023-04-25 11:28:24 +0200

    setting up prisma/fetching blogs from backend

--- app/(home)/Trending.tsx
+++ app/(home)/Trending.tsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
+import { Post } from "@prisma/client";
 
-export default function Trending() {
+interface TrendingProps {
+  trendingPosts: Post[];
+}
+
+export default function Trending({trendingPosts}: TrendingProps) {
   return (
     <section className="pt-3 pb-10 ">
       <div className="flex items-center gap-3">
@@ -28,10 +33,10 @@ export default function Trending() {
         </div>
       </div> */}
       <div className="sm:grid grid-cols-4 gap-5 grid-rows-2 sm:h-[600px] my-3">
-        <TrendingCard className=" col-span-2 row-span-2 border" />
-        <TrendingCard className=" col-span-2 row-span-1 border" />
-        <TrendingCard className=" col-span-1 row-span-1 border" />
-        <TrendingCard className=" col-span-1 row-span-1 border" />
+        <TrendingCard className=" col-span-2 row-span-2 border" post = {trendingPosts[0]}/>
+        <TrendingCard className=" col-span-2 row-span-1 border" post = {trendingPosts[1]}/>
+        <TrendingCard className=" col-span-1 row-span-1 border" post = {trendingPosts[2]}/>
+        <TrendingCard className=" col-span-1 row-span-1 border" post = {trendingPosts[3]}/>
       </div>
       <p className="text-sm">
         Lorem ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -46,20 +51,21 @@ export default function Trending() {
 
 type TrendingCardProps = {
   className?: string;
+  post:Post
 };
 
-const TrendingCard = ({ className }: TrendingCardProps) => {
+const TrendingCard = ({ className,post }: TrendingCardProps) => {
   return (
     <Link
-      href="/"
+    
       className={`${className} sm:mt-0 sm:h-auto relative mt-7 block w-full h-96 hover:opacity-70`}
-      //   href={`${process.env.NEXT_PUBLIC_URL}/post/${post?.id}`}
+        href={`${process.env.NEXT_PUBLIC_URL}/post/${post?.id}`}
     >
       <div className="z-0 relative w-full h-full">image</div>
       <div className="absolute z-1 top-0 left-0 w-full h-full bg-gradient-gradual" />
       <div className="absolute z-2 bottom-0 left-0 p-3">
         <h4 className="inline-block px-5 py-1 font-semibold bg-accent-orange text-wh-900">
-          dfg
+         {post.category}
         </h4>
         <div className="text-wh-100 mt-2">dfg</div>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -6,24 +6,61 @@ import Travel from "./(home)/Travel";
 import Other from "./(shared)/Other";
 import Subscribe from "./(shared)/Subscribe";
 import Sidebar from "./(shared)/Sidebar";
-
+import { prisma } from "./api/hello/client";
+import { Post } from "@prisma/client";
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+const getPosts = async () => {
+  const posts: Post[] = await prisma.post.findMany();
+  return posts;
+};
+
+export default async function Home() {
+  const posts = await getPosts();
+  console.log(posts);
+
+  const formatPosts = () => {
+    const trendingPosts: Post[] = [];
+    const techPosts: Post[] = [];
+    const travelPosts: Post[] = [];
+    const otherPosts: Post[] = [];
+
+    posts.forEach((post: Post, i: number) => {
+      if (i < 4) {
+        trendingPosts.push(post);
+      }
+      if (post?.category === "Tech") {
+        techPosts.push(post);
+      } else if (post?.category === "Travel") {
+        travelPosts.push(post);
+      } else if (
+        post?.category === "Interior Design" ||
+        post?.category === "Fashion" ||
+        post?.category === "Food"
+      ) {
+        otherPosts.push(post);
+      } 
+     
+    });
+    return [trendingPosts, techPosts, travelPosts, otherPosts];
+  };
+
+  const [ trendingPosts, techPosts, travelPosts, otherPosts ] = formatPosts();
+
   return (
     <main className="px-10 leading-7 maincol">
-      <Trending />
+      <Trending trendingPosts= {trendingPosts}/>
       <div className="md:flex gap-10 mb-5">
         <div className="basis-3/4">
           <Tech />
           <Travel />
-          <Other/>
+          <Other />
           <div className="hidden md:block ">
-            <Subscribe/>
+            <Subscribe />
           </div>
         </div>
         <div className="basis-1/4">
-          <Sidebar/>
+          <Sidebar />
         </div>
       </div>
     </main>
